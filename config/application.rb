4d3b64ab7715aa5b595d365931c916678c93814b
kumamonmaster <kumamonmaster@gmail.com>
2016-09-27 13:12:25 +0000

    Edit application.rb

--- config/application.rb
+++ config/application.rb
@@ -21,5 +21,15 @@ module TapCho
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+
+    config.time_zone = 'Tokyo'
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
+    config.i18n.default_locale = :ja
+
+    config.generators do |g|
+      g.assets false
+      g.helper false
+      g.template_engine :slim
+    end
   end
 end
