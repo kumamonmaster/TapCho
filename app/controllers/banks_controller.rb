fa9175b366a3449fadebb48e46c2d58bc2092c42
kumamonmaster <kumamonmaster@gmail.com>
2016-09-28 06:45:44 +0000

    Add create for bank

--- app/controllers/banks_controller.rb
+++ app/controllers/banks_controller.rb
@@ -1,5 +1,6 @@
 class BanksController < ApplicationController
   before_action :authenticate_user!
+
   def index
     @banks = current_user.banks.all
   end
@@ -10,4 +11,18 @@ class BanksController < ApplicationController
   def new
     @bank = Bank.new
   end
+
+  def create
+    @bank = current_user.banks.build(bank_params)
+    if @bank.save
+      redirect_to user_banks_path
+    else
+      render :new
+    end
+  end
+
+  private
+    def bank_params
+      params.require(:bank).permit(:title, :goal, :user_id)
+    end
 end
