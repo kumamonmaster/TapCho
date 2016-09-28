4d3da32115b6b8479c52ca1b3e422c9b7b4d944e
kumamonmaster <kumamonmaster@gmail.com>
2016-09-28 04:59:55 +0000

    Add assosiation and validation to Bank

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,8 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   attr_accessor :login
-
+  has_many :banks
+  
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable,
          :authentication_keys => [:login]
